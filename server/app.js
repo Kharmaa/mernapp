b299fb3607c169cab777828019696d10002b13f9
Kharmaa <riikka.karmala@outlook.com>
2026-01-25 18:04:17 +0200

    fixing render issue

--- server/app.js
+++ server/app.js
@@ -51,13 +51,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "Unknown error" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
