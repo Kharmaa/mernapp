0dc3c4eca0938125d75064a06d04cb251c7b035f
Kharmaa <riikka.karmala@outlook.com>
2026-01-26 20:57:35 +0200

    update

--- server/app.js
+++ server/app.js
@@ -53,14 +53,11 @@ app.use((error, req, res, next) => {
 
 const PORT = process.env.PORT || 10000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
